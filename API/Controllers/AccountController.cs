2cec3e95fa2a921c661f3fc200908454bcd163b1
neerajrkanojiya <34946903+neerajrkanojiya@users.noreply.github.com>
2021-10-07 15:52:05 +0530

    Session-39

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
-using API.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using API.Entities;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.DTOs;
 using System.Security.Cryptography;
 using System.Text;
-using API.DTOs;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-
         public AccountController(DataContext context)
         {
             _context = context;
@@ -30,13 +24,31 @@ namespace API.Controllers
 
             var user=new Appuser{
                 UserName=registerDTO.Username.ToLower(),
-                PasswordHash=hmac.ComputeHash(Encoding.UTF32.GetBytes(registerDTO.Password)),
+                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt=hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
+            return user;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<Appuser>>Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if(user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0; i<computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
             return user;
         }
         public async Task<bool> UserExists(string username)
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,7 +2,6 @@ using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using API.Entities;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
